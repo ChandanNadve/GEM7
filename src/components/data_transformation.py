eff705fc0cfc66687c40a98508d622df0bc11c27
CHANDAN PRAKASH <chandannadve@gmail.com>
2023-08-18 15:50:35 +0400

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -15,19 +15,21 @@ from sklearn.preprocessing import OrdinalEncoder # Ordinal Encoding
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 
+from src.utils import save_object
+
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifcats','preprocessor.pkl')
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
     
 class DataTransformation:
     def __init__(self):
-        self.DataTransformationConfig()
+        self.data_transformation_config=DataTransformationConfig()
     
     def get_data_transformation_object(self):
         try:
             logging.info("Data Trnasirmation is intiated")
             
-            catergorical_cols=['cut','color','clarity']
+            categorical_cols=['cut','color','clarity']
             numerical_cols=['carat','depth','table','x','y','z']
             
             cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
@@ -76,16 +78,37 @@ class DataTransformation:
             
             logging.info('Obtaining preprocessing object')
             
+            preprocessing_obj=self.get_data_transformation_object()
+            
             target_column_name='price'
             drop_columns=[target_column_name,'id']
             
             input_feature_train_df=train_df.drop(columns=drop_columns,axis=1)
             target_feature_train_df=train_df[target_column_name]
             
-            input_featured_test_df=test_df.drop(columns=drop_columns,axis=1)
+            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
             target_feature_test_df=test_df[target_column_name]
             
-            preprocessing_obj=self.get_data_transformation_object()
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            
+            logging.info("Applying preprocessing object on train and split datasets.")
+            
+            train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+            
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            
+            logging.info('Preprocessor pickle file saved')
+            
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
             
         except Exception as e:
             logging.info("Exception occured in the initiate_data_transformation")
--- src/components/test.py
+++
@@ -1,1 +0,0 @@
-from src.loggers import logging
